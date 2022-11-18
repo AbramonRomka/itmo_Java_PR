6154b1ad6b5fb6157088ce72b4143e5441f1fe3a
Roman <abramonromka@gmail.com>
2022-11-18 21:56:12 +0300

    space remove

--- src/itmo/java/basic/lesson1/Main.java
+++ src/itmo/java/basic/lesson1/Main.java
@@ -4,27 +4,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Задача 1
+        //Задача 1
         System.out.println("Я");
         System.out.println("хорошо");
         System.out.println("знаю");
         System.out.println("Java");
 
-        // Задача 2
+        //Задача 2
         System.out.println((46 + 10) * (10 / 3));
         System.out.println(29 * 4 * (-15));
 
-        // Задача 3
+        //Задача 3
         double number = 10500;
         System.out.println((number / 10d) / 10d);
 
-        // Задача 4
+        //Задача 4
         double a = 3.6;
         double b = 4.1;
         double c = 5.9;
         System.out.println(a * b * c);
 
-        // Задача 5
+        //Задача 5
         Scanner console = new Scanner(System.in);
         int x = console.nextInt();
         int y = console.nextInt();
@@ -35,7 +35,7 @@ public class Main {
         System.out.println(z);
 
 
-        // Задача 6
+        //Задача 6
         Scanner console1 = new Scanner(System.in);
         int n = console1.nextInt();
         if ((n % 2) == 0) {
