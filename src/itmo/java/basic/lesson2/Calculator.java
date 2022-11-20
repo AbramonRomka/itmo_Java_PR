256d15ac8fb0ffc119d68e9c22d21aef75f44ca6
Roman <abramonromka@gmail.com>
2022-11-20 15:37:02 +0300

    Task 2 v.3 (fix)

--- src/itmo/java/basic/lesson2/Calculator.java
+++ src/itmo/java/basic/lesson2/Calculator.java
@@ -2,61 +2,50 @@ package itmo.java.basic.lesson2;
 
 public class Calculator {
     public static void main(String[] args){
-        int a = 2;
-        int b = 3;
+        long a = 2;
+        long b = 3;
         Calculator calc = new Calculator();
 
         System.out.println(calc.division(a,b));
     }
     int addition(int a, int b) {
-        int c = a+b;
-        return c;
+        return (a+b);
+
     }
     double addition(double a, double b) {
-        double c = a+b;
-        return c;
+        return (a+b);
     }
     long addition(long a, long b) {
-        long c = a+b;
-        return c;
+        return (a+b);
     }
 
     int subtraction(int a, int b) {
-        int c = a-b;
-        return c;
+        return (a-b);
     }
     double subtraction(double a, double b) {
-        double c = a-b;
-        return c;
+        return (a-b);
     }
     long subtraction(long a, long b) {
-        long c = a-b;
-        return c;
+        return (a-b);
     }
 
     double division(int a, int b) {
-        double c = (double) a/b;
-        return c;
+        return (double) a/b;
     }
     double division(double a, double b) {
-        double c = a/b;
-        return c;
+        return (a/b);
     }
-    long division(long a, long b) {
-        long c = a/b;
-        return c;
+    double division(long a, long b) {
+        return (double) a/b;
     }
 
     int multiplication(int a, int b) {
-        int c = a*b;
-        return c;
+        return (a*b);
     }
     double multiplication(double a, double b) {
-        double c = a*b;
-        return c;
+        return (a*b);
     }
     long multiplication(long a, long b) {
-        long c = a*b;
-        return c;
+        return (a*b);
     }
 }
