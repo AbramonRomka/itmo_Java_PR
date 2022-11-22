62f6d83acbac6ba1e8ab0f308a3b96badd53db6f
Roman <abramonromka@gmail.com>
2022-11-22 19:36:56 +0300

    Task 3 fix (add getter and setter)

--- src/itmo/java/basic/lesson3/Car.java
+++ src/itmo/java/basic/lesson3/Car.java
@@ -1,9 +1,9 @@
 package itmo.java.basic.lesson3;
 
 public class Car {
-    String color;
-    String model;
-    int weight;
+    private String color;
+    private String model;
+    private int weight;
 
     void print(String color, String model, int weight) {
         System.out.println("Цвет: " + color + " Модель: " + model + " Вес: " + weight);
@@ -22,18 +22,39 @@ public class Car {
 
     }
 
+    public String getColor(String color){
+        return color;
+    }
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    public String getModel(String model){
+        return model;
+    }
+    public  void setModel(String model){
+        this.model = model;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
 
     public static void main(String[] args) {
         Car car1 = new Car();
         Car car2 = new Car();
 
-        car1.color = "Красный";
-        car1.model = "BMW";
-        car1.weight = 1200;
+        car1.setColor("Красный");
+        car1.setModel("BMW");
+        car1.setWeight(1200);
 
-        car2.color = "Белый";
-        car2.model = "Mercedes";
-        car2.weight = 1600;
+        car2.setColor("Белый");
+        car2.setModel("Mercedes");
+        car2.setWeight(1600);
 
         car1.print(car1.color, car1.model, car1.weight);
         car2.print(car2.color, car2.model, car2.weight);
